ada59cf1af6f5254e914160080641cd0ff0d69c3
NICHOLAS-AGBO <ndcconcept@gmail.com>
2023-02-13 19:45:17 +0100

    Update Staking.tsx

--- components/Staking.tsx
+++ components/Staking.tsx
@@ -1,21 +1,27 @@
 import React, {FC} from "react";
-import {Container, Paper, Typography, Unstable_Grid2 as Grid} from "@mui/material";
+import {Box, Container, IconButton, Paper, Stack, Typography, Unstable_Grid2 as Grid} from "@mui/material";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@static/img/logo.png";
+import {Menu} from "@mui/icons-material";
 
 const Staking:FC = () => {
   return(
       <>
       <Container maxWidth={false} sx={{py: 5}}>
         <Grid container gap={3} minHeight={"90vh"}>
-          <Grid xs={0.5} md={2.5} sx={{overflow: "hidden"}}>
-              <Typography variant={"h5"} color={"primary"} mb={2}>
-                  <Link href={"/"}>
-                      <Image style={{verticalAlign: "middle"}} src={logo} alt={"stakersnet logo"} priority width={30} height={30}/>&nbsp;
-                      Stakersnet
-                  </Link>
-              </Typography>
+          <Grid xs={12} md={2.5} sx={{overflow: "hidden"}}>
+              <Stack flexDirection={"row"} alignItems={"center"} gap={2}>
+                  <Box sx={{width: "max-content", p: .25, backgroundColor: "rgb(51,150,255)", color: "common.white", borderRadius: 2.5}}>
+                      <IconButton color={"inherit"}><Menu color={"inherit"}/></IconButton>
+                  </Box>
+                  <Typography variant={"h5"} color={"primary"}>
+                      <Link href={"/"}>
+                          <Image style={{verticalAlign: "middle"}} src={logo} alt={"stakersnet logo"} priority width={30} height={30}/>&nbsp;
+                          Stakersnet
+                      </Link>
+                  </Typography>
+              </Stack>
           </Grid>
 
           <Grid flexGrow={1}>
